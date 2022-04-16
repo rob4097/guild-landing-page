cf4bd7ebfce92385f61c0a93bc7ccc434212959e
Robert Powers <rob_40@live.com>
2022-04-16 16:01:46 -0500

    Update .eleventy.js
    https://github.com/11ty/eleventy/issues/950#issuecomment-588980890

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,9 @@
 const markdownShortcode = require("eleventy-plugin-markdown-shortcode");
 
 module.exports = function (eleventyConfig) {
+    eleventyConfig.setLiquidOptions({
+        dynamicPartials: true
+    });
     eleventyConfig.addPlugin(markdownShortcode);
     eleventyConfig.setTemplateFormats([
         'html',
